2ea0894a0c8a1eb6f78bca682d95584cb14d81f0
haedong31 <haedong.kim90@gmail.com>
2024-06-25 13:42:38 -0400

    get_methyl_adj for intervals

--- fun_4Grace.py
+++ fun_4Grace.py
@@ -2,21 +2,31 @@ import hail as hl
 import pandas as pd
 from pathlib import Path
 
-# hl.init()
-
-def get_methyl_adj(chrom, pos, context, ref, alt, context_ht, methyl_adj_tbl=None):
+def get_methyl_adj(chrom, start, end, context_ht=None, methyl_adj_tbl=None):
     data_dir = Path('/mnt/isilon/immgen_res/data/gnomad-nc-constraint-v31-paper')
     if methyl_adj_tbl is None:
         methyl_adj_tbl = pd.read_csv(data_dir/'methyl_lookup.txt', sep='\t')
-    
-    locus_expr = hl.locus(chrom, pos, reference_genome='GRCh38')
-    context_4input = context_ht.filter((context_ht.context == context) & (context_ht.ref == ref) & (context_ht.alt == alt) & (context_ht.locus == locus_expr))
+        if 'methylation_level' in methyl_adj_tbl.columns:
+            methyl_adj_tbl.rename(columns={'methylation_level':'methyl_level'}, inplace=True)
 
-    methyl_level = context_4input.aggregate(hl.agg.collect(context_4input.methyl_level))
-    methyl_level = methyl_level[0]
-    row_slice = methyl_adj_tbl[(methyl_adj_tbl['context'] == context) & (methyl_adj_tbl['ref'] == ref) & (methyl_adj_tbl['alt'] == alt) & (methyl_adj_tbl['methylation_level'] == methyl_level)]
+    if context_ht is None:
+        hl.init()
+        context_ht = hl.read_table(str(data_dir/'context_prefiltered.ht'))
 
-    row_slice = row_slice[['context', 'ref', 'alt', 'methylation_level', 'methyl_factor']].reset_index()
-    row_pos = pd.DataFrame({'chrom': chrom, 'pos':pos}, index=[0])
+    intervals = [hl.parse_locus_interval(f'{chrom}:{str(start)}-{str(end)}', reference_genome='GRCh38')]
+    context_sub_df = hl.filter_intervals(context_ht, intervals)
+    context_sub_df = context_sub_df.select(
+        context_sub_df.context,
+        context_sub_df.ref,
+        context_sub_df.alt,
+        context_sub_df.methyl_level
+    ).to_pandas()
+    group_vars = ['context','ref','alt','methyl_level']
+
+    context_sub_df['locus'] = context_sub_df['locus'].astype(str)
+    context_sub_df[['chrom','pos']] = context_sub_df['locus'].str.split(':', expand=True)
+    context_sub_df.drop(columns='locus', inplace=True)
+    context_sub_df = context_sub_df[['chrom','pos','alleles']+group_vars]
     
-    return pd.concat([row_pos, row_slice], axis=1)
+    return pd.merge(context_sub_df, methyl_adj_tbl[group_vars+['methyl_factor']], how='left', on=group_vars)
+
